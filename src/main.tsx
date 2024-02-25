eb69cf6cb2e5345e2632a5ace5f1d1f8214a42e2
ANDI MARTIN GALAN <andi.martingalan@colaborador.elcorteingles.es>
2024-02-26 00:01:36 +0100

    fix type mismatch

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+
+if (rootElement){
+  const root = ReactDOM.createRoot(rootElement as HTMLElement);
+  root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+  );
+} 
